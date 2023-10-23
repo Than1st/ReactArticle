f083fb296c630f6b8e9456e43e7cddf7ed1647ed
Sulthan Laksono Ramadhan <1911500641@student.budiluhur.ac.id>
2023-10-23 17:16:35 +0700

    Change ContactUs.js

--- client/src/components/ContactUs.js
+++ client/src/components/ContactUs.js
@@ -1,27 +1,56 @@
 import {useState} from "react";
+import {BsGithub, BsInstagram} from "react-icons/bs";
+import {AiOutlineLinkedin} from "react-icons/ai";
+import {IoNavigate} from "react-icons/io5";
 
 export const ContactUs = () => {
     const [image, setImage] = useState('https://via.placeholder.com/100')
     return (
         <div className='container pt-5'>
-            <div className='card'>
+            <div className='card w-50'>
                 <div className='card-header'>
                     <h3>Contact Us</h3>
                 </div>
                 <div className='card-body'>
-                    <form>
-                        <input type='text' className='form-control mb-2' name='title' placeholder='Title' minLength='10' required/>
-                        <textarea name='content' className='form-control mb-2' placeholder='Content' minLength='100' required></textarea>
-                        <img src={image} style={{width: "150px", height: "auto"}} alt='' className='mb-2'/>
-                        <input type='text' className='form-control mb-2' name='title' placeholder='Link Image' minLength='10' required/>
-                        <div className="form-check mb-2">
-                            <input className="form-check-input" type="checkbox" id="inlineFormCheck"/>
-                            <label className="form-check-label" htmlFor="inlineFormCheck">
-                                Post Now
-                            </label>
+                    <a href='https://www.instagram.com/thanfpv/' target='_blank'>
+                        <div className='mb-2 d-flex'>
+                            <div>
+                                <BsInstagram size={50}/>
+                            </div>
+                            <div className='d-flex ps-2 align-items-center'>
+                                <h3>thanfpv</h3>
+                            </div>
+                            <div className='d-flex ps-2 '>
+                                <IoNavigate size={25}/>
+                            </div>
                         </div>
-                        <button type='submit' className='btn btn-primary w-100'>Submit</button>
-                    </form>
+                    </a>
+                    <a href='https://www.linkedin.com/in/thanfirst/' target='_blank'>
+                        <div className='mb-2 d-flex'>
+                            <div>
+                                <AiOutlineLinkedin size={50}/>
+                            </div>
+                            <div className='d-flex ps-2 align-items-center'>
+                                <h3>Sulthan Laksono Ramadhan</h3>
+                            </div>
+                            <div className='d-flex ps-2 '>
+                                <IoNavigate size={25}/>
+                            </div>
+                        </div>
+                    </a>
+                    <a href='https://github.com/Than1st' target='_blank'>
+                        <div className='mb-2 d-flex'>
+                            <div>
+                                <BsGithub size={50}/>
+                            </div>
+                            <div className='d-flex ps-2 align-items-center'>
+                                <h3>Than1st</h3>
+                            </div>
+                            <div className='d-flex ps-2 '>
+                                <IoNavigate size={25}/>
+                            </div>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
